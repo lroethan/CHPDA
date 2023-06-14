cd6da0e5bfb334841cb6b80c76dc584f7892728c
Ethan <panethan28@gmail.com>
2023-06-14 23:16:03 +0800

    update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Cross-granularity Hybrid Physical Design Advisor with Deep Reinforcement Learning
 
-### 1. Preparation
+###  Preparation
 
 1. Refer to the TiDB Quickstart guide located at [TiDB Quickstart](https://docs.pingcap.com/tidb/dev/quick-start-with-tidb) for the necessary pre-requisites before installing and running TiUP.
 2. Run the following command to launch a TiUP playground environment with one instance of TiDB (--db 1), one instance of PD (--pd 1), and the latest nightly version of TiKV (--kv nightly):
--- entry/main.py
+++ entry/main.py
@@ -8,36 +8,54 @@ import util.tidb_connector as ti_conn
 
 
 def run_dqn(is_fix_count, hyperparameter, x, is_dnn, is_ps, is_double, a):
+    # Set name for experiment
     hyperparameter['NAME'] = f"TEST_{x}"
+
+    # Load workload and candidate indexes
     print("Loading workload...")
     with open("entry/workload.pickle", "rb") as wf:
         workload = pickle.load(wf)
-    print("Loading candidate...")
+    print("Loading candidate indexes...")
     with open("entry/cands.pickle", "rb") as cf:
         index_candidates = pickle.load(cf)
-    print(workload)
+
+    # Choose the appropriate DQN agent
     if is_fix_count:
         agent = dqn_fc.DQN(workload[:], index_candidates, "hypo", hyperparameter, is_dnn, is_ps, is_double, a)
     else:
         agent = dqn_fs.DQN(workload, index_candidates, "hypo", hyperparameter)
+
+    # Train the agent and select the indexes
     indexes = agent.train(False, x)
     selected_indexes = [index_candidates[i] for i, idx in enumerate(indexes) if idx == 1.0]
+
     return selected_indexes
 
 
 def get_performance(selected_indexes, frequencies):
-    print(frequencies)
+    # Normalize the frequencies
     frequencies = np.array(frequencies) / np.array(frequencies).sum()
+
+    # Load the workload and connect to TiDB
     with open("entry/workload.pickle", "rb") as wf:
         workload = pickle.load(wf)
     tidb_client = ti_conn.TiDBDatabaseConnector("tpch")
-    cost1 = (np.array(tidb_client.get_queries_cost(workload)) * frequencies).sum()
-    print(cost1)
+
+    # Calculate the original cost
+    original_cost = (np.array(tidb_client.get_queries_cost(workload)) * frequencies).sum()
+    print(f"Original cost: {original_cost}")
+
+    # Create the hypothetical indexes and calculate the new cost
     for index in selected_indexes:
         tidb_client.execute_create_hypo(index)
-    cost2 = (np.array(tidb_client.get_queries_cost(workload)) * frequencies).sum()
-    print(cost2)
-    print((cost1 - cost2) / cost1)
+    new_cost = (np.array(tidb_client.get_queries_cost(workload)) * frequencies).sum()
+    print(f"New cost: {new_cost}")
+
+    # Calculate the cost reduction and return it
+    cost_reduction = (original_cost - new_cost) / original_cost
+    print("===============COST REDUCTION=================")
+    return cost_reduction
+
 
 
 # conf21 = {
@@ -59,7 +77,7 @@ conf21 = {
     "U_ITERATION": 5,
     "BATCH_SIZE": 64,
     "GAMMA": 0.95,
-    "EPISODES": 20,
+    "EPISODES": 50,
     "LEARNING_START": 1000,
     "MEMORY_CAPACITY": 20000,
 }
@@ -97,4 +115,4 @@ def entry(is_fix_count: bool, constraint):
     frequencies = [1659, 1301, 1190, 1741, 1688, 1242, 1999, 1808, 1433, 1083, 1796, 1266, 1046, 1353]
     print(get_performance(selected_indexes, frequencies))
 
-entry(True, 5)
+entry(True, 3)
